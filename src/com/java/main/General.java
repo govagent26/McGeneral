b48b78aead27457ff5d7fc972dd2cdffb688047c
Nash W <nash26@optonline.net>
2011-08-14 17:00:13 -0400

    third commit (practice)

--- src/com/java/main/General.java
+++ src/com/java/main/General.java
@@ -1,5 +1,16 @@
 package com.java.main;
 
-public class General {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class General extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onEnable() {
+		
+	}
 }
