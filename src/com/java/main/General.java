9dd3ae3c3bcddc4c1f2a5abfac99b381ed4a0e7a
Nash W <nash26@optonline.net>
2011-08-14 17:37:22 -0400

    starting to program additional classes

--- src/com/java/main/General.java
+++ src/com/java/main/General.java
@@ -1,5 +1,9 @@
 package com.java.main;
 
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class General extends JavaPlugin {
@@ -11,6 +15,17 @@ public class General extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
+		
+		registerEvents();
+		
+	}
+	
+	private void registerEvents() {
+		PlayerListener pL = new McPlayerListener(this);
+		
+		PluginManager pm = getServer().getPluginManager();
+    	
+    	pm.registerEvent(Event.Type.PLAYER_JOIN, pL, Priority.Normal, this);
 		
 	}
 }
